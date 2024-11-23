30559d0b96b649a21fd32a75978ecb2ad8861723
Electr1k <110545080+Electr1k@users.noreply.github.com>
2024-11-23 17:01:43 +0300

    add image

--- KGLaba4/Form1.cs
+++ KGLaba4/Form1.cs
@@ -11,43 +11,320 @@ namespace KGLaba4
         Bitmap bitmap;
         Graphics graphics;
         int currentLayout = 0;
-        int scale = 5;
+        int scale = 3;
 
         public Form1()
         {
             InitializeComponent();
             image = new Image(new Layout(Color.Red, Color.Black, new List<Point> { new Point(0, 0), new Point(40, 0), new Point(40, 40), new Point(0, 40) }));
 
-            List<Point> l1 = new List<Point>
-        {
-            new Point(5, 5),
-            new Point(20, 5),
-            new Point(20, 20),
 
-            new Point(5, 20),
-        };
+            List<Point> l1 = new List<Point>
+{
+    new Point(13 * 5, 23 * 5),
+    new Point(20 * 5, 14 * 5),
+    new Point(20 * 5, 23 * 5),
+};
 
             List<Point> l2 = new List<Point>
-        {
-            new Point(3, 30),
-            new Point(20, 1),
-            new Point(40, 30),
-        };
+{
+    new Point(13 * 5, 23 * 5),
+    new Point(13 * 5, 13 * 5),
+    new Point(20 * 5, 14 * 5),
+};
             List<Point> l3 = new List<Point>
-        {
-            new Point(5, 30),
-            new Point(30, 5),
-            new Point(35, 30),
-        };
+{
+    new Point(4 * 5, 22 * 5),
+    new Point(13 * 5, 13 * 5),
+    new Point(13 * 5, 23 * 5),
+};
+            List<Point> l4 = new List<Point>
+{
+    new Point(13 * 5, 13 * 5),
+    new Point(19 * 5, 7 * 5),
+    new Point(20 * 5, 14 * 5),
+};
 
+            List<Point> l5 = new List<Point>
+{
+    new Point(13 * 5, 13 * 5),
+    new Point(17 * 5, 6 * 5),
+    new Point(19 * 5, 7 * 5),
+};
+            List<Point> l6 = new List<Point>
+{
+    new Point(19 * 5, 7 * 5),
+    new Point(17 * 5, 6 * 5),
+    new Point(19 * 5, 5 * 5),
+};
+            List<Point> l7 = new List<Point>
+{
+    new Point(13 * 5, 13 * 5),
+    new Point(14 * 5, 5 * 5),
 
-            Layout layout1 = new Layout(Color.Red, Color.Black, l1);
+    new Point(17 * 5, 6 * 5),
+};
+
+            List<Point> l8 = new List<Point>
+{
+    new Point(17 * 5, 6 * 5),
+
+    new Point(18 * 5, 1 * 5),
+    new Point(20 * 5, 5 * 5),
+
+};
+            List<Point> l9 = new List<Point>
+{
+    new Point(14 * 5, 5 * 5),
+    new Point(18 * 5, 1 * 5),
+    new Point(17 * 5, 6 * 5),
+};
+            List<Point> l10 = new List<Point>
+{
+    new Point(14 * 5, 5 * 5),
+    new Point(12 * 5, 4 * 5),
+    new Point(16 * 5, 4 * 5),
+};
+
+            List<Point> l11 = new List<Point>
+{
+    new Point(13 * 5, 13 * 5),
+    new Point(4 * 5, 12 * 5),
+    new Point(14 * 5, 5 * 5),
+};
+
+            List<Point> l12 = new List<Point>
+{
+    new Point(7 * 5, 19 * 5),
+    new Point(6 * 5, 15 * 5),
+    new Point(13 * 5, 13 * 5),
+};
+
+            List<Point> l13 = new List<Point>
+{
+    new Point(7 * 5, 19 * 5),
+    new Point(4 * 5, 16 * 5),
+    new Point(6 * 5, 15 * 5),
+};
+
+            List<Point> l14 = new List<Point>
+{
+    new Point(4 * 5, 16 * 5),
+
+    new Point(4 * 5, 12 * 5),
+    new Point(13 * 5, 13 * 5),
+};
+            List<Point> l15 = new List<Point>
+{
+    new Point(4 * 5, 12 * 5),
+    new Point(6 * 5, 5 * 5),
+    new Point(11 * 5, 7 * 5),
+};
+            List<Point> l16 = new List<Point>
+{
+    new Point(11 * 5, 7 * 5),
+    new Point(6 * 5, 5 * 5),
+
+    new Point(12 * 5, 4 * 5),
+};
+            List<Point> l17 = new List<Point>
+{
+    new Point(11 * 5, 7 * 5),
+    new Point(12 * 5, 4 * 5),
+
+    new Point(14 * 5, 5 * 5),
+};
+
+            List<Point> l18 = new List<Point>
+{
+    new Point(7 * 5, 5 * 5),
+    new Point(9 * 5, 2 * 5),
+
+    new Point(12 * 5, 4 * 5),
+};
+
+            List<Point> l19 = new List<Point>
+{
+    new Point(16 * 5, 5 * 5 - 1),
+    new Point(18 * 5, 2 * 5),
+
+    new Point(19 * 5 - 1, 5 * 5 - 1),
+};
+
+            List<Point> l20 = new List<Point>
+{
+    new Point(6 * 5, 13 * 5 + 1 ),
+    new Point(10 * 5, 13 * 5 + 1),
+
+    new Point(8 * 5, 14 * 5),
+};
+
+            List<Point> l21 = new List<Point>
+{
+    new Point(5 * 5 + 1, 13 * 5 -1 ),
+    new Point(5 * 5 + 1, 11 * 5 - 1),
+
+    new Point(7 * 5 + 1, 13 * 5 - 1 ),
+};
+
+            List<Point> l22 = new List<Point>
+{
+    new Point(7 * 5 + 1, 13 * 5 - 1 ),
+
+    new Point(10 * 5 + 1, 11 * 5 -1 ),
+    new Point(10 * 5 + 1, 13 * 5 - 1),
+
+};
+            List<Point> l23 = new List<Point>
+{
+    new Point(7 * 5 + 1, 13 * 5 - 1 ),
+    new Point(5 * 5 + 1, 11 * 5 - 1),
+    new Point(7 * 5 + 1, 10 * 5 - 1),
+};
+            List<Point> l24 = new List<Point>
+{
+    new Point(7 * 5 + 1, 13 * 5 - 1 ),
+    new Point(7 * 5 + 1, 10 * 5 - 1),
+
+    new Point(10 * 5 + 1, 11 * 5 - 1),
+};
+
+            List<Point> l25 = new List<Point>
+{
+    new Point(7 * 5 + 1, 13 * 5 - 1 ),
+    new Point(6 * 5 + 1, 11 * 5 - 1),
+    new Point(7 * 5 + 1, 10 * 5 - 1),
+};
+            List<Point> l26 = new List<Point>
+{
+    new Point(7 * 5 + 1, 13 * 5 - 1 ),
+    new Point(7 * 5 + 1, 10 * 5 - 1),
+
+    new Point(8 * 5 + 1, 11 * 5 - 1),
+};
+
+            List<Point> l27 = new List<Point>
+{
+    new Point(7 * 5, 9 * 5 ),
+    new Point(6 * 5, 8 * 5),
+
+    new Point(7 * 5, 7 * 5),
+};
+
+            List<Point> l28 = new List<Point>
+{
+    new Point(7 * 5, 9 * 5 ),
+    new Point(7 * 5, 7 * 5),
+
+    new Point(8 * 5, 8 * 5),
+};
+
+            List<Point> l29 = new List<Point>
+{
+    new Point(7 * 5 - 2, 8 * 5 ),
+    new Point(7 * 5 , 8 * 5 - 2),
+
+    new Point(8 * 5 - 2, 8 * 5),
+};
+
+            List<Point> l30 = new List<Point>
+{
+    new Point(12 * 5, 10 * 5 ),
+    new Point(11 * 5, 9 * 5),
+
+    new Point(12 * 5, 8 * 5),
+};
+
+            List<Point> l31 = new List<Point>
+{
+    new Point(12 * 5, 10 * 5 ),
+    new Point(12 * 5, 8 * 5),
+
+    new Point(14 * 5, 9 * 5 ),
+};
+
+            List<Point> l32 = new List<Point>
+{
+    new Point(12 * 5, 9 * 5),
+    new Point(12 * 5 + 2, 9 * 5 - 2),
+
+    new Point(13 * 5 - 2, 9 * 5),
+};
+            Layout layout1 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l1);
             image.Add(layout1);
-            Layout layout2 = new Layout(Color.Green, Color.Black, l2);
+            Layout layout2 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l2);
             image.Add(layout2);
-            Layout layout3 = new Layout(Color.Blue, Color.Black, l3);
-            //image.Add(layout3);
-            Console.WriteLine("In polygon " + needVisable(layout2, new Point(17, 10)));
+            Layout layout3 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l3);
+            image.Add(layout3);
+            Layout layout4 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l4);
+            image.Add(layout4);
+
+            Layout layout5 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l5);
+            image.Add(layout5);
+
+            Layout layout6 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l6);
+            image.Add(layout6);
+            Layout layout7 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l7);
+            image.Add(layout7);
+            Layout layout8 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l8);
+            image.Add(layout8);
+            Layout layout9 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l9);
+            image.Add(layout9);
+            Layout layout10 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l10);
+            image.Add(layout10);
+            Layout layout11 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l11);
+            image.Add(layout11);
+            Layout layout12 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l12);
+            image.Add(layout12);
+            Layout layout13 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l13);
+            image.Add(layout13);
+            Layout layout14 = new Layout(Color.FromArgb(255, 247, 202), Color.Black, l14);
+            image.Add(layout14);
+            Layout layout15 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l15);
+            image.Add(layout15);
+            Layout layout16 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l16);
+            image.Add(layout16);
+            Layout layout17 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l17);
+            image.Add(layout17);
+
+            Layout layout18 = new Layout(Color.FromArgb(244, 200, 111), Color.Black, l18);
+            image.Add(layout18);
+            Layout layout19 = new Layout(Color.FromArgb(247, 213, 150), Color.Black, l19);
+            image.Add(layout19);
+            Layout layout20 = new Layout(Color.Black, Color.Black, l20);
+            image.Add(layout20);
+            Layout layout21 = new Layout(Color.FromArgb(255, 247, 202), Color.Black, l21);
+            image.Add(layout21);
+
+            Layout layout22 = new Layout(Color.FromArgb(255, 247, 202), Color.Black, l22);
+            image.Add(layout22);
+            Layout layout23 = new Layout(Color.FromArgb(255, 247, 202), Color.Black, l23);
+            image.Add(layout23);
+            Layout layout24 = new Layout(Color.FromArgb(255, 247, 202), Color.Black, l24);
+            image.Add(layout24);
+            Layout layout25 = new Layout(Color.Black, Color.Black, l25);
+            image.Add(layout25);
+            Layout layout26 = new Layout(Color.Black, Color.Black, l26);
+            image.Add(layout26);
+            
+            Layout layout27 = new Layout(Color.White, Color.Black, l27);
+            image.Add(layout27);
+
+            Layout layout28 = new Layout(Color.White, Color.Black, l28);
+            image.Add(layout28);
+
+            Layout layout29 = new Layout(Color.Black, Color.Black, l29);
+            image.Add(layout29);
+
+            Layout layout30 = new Layout(Color.White, Color.Black, l30);
+            image.Add(layout30);
+
+            Layout layout31 = new Layout(Color.White, Color.Black, l31);
+            image.Add(layout31);
+
+            Layout layout32 = new Layout(Color.Black, Color.Black, l32);
+            image.Add(layout32);
+            //Console.WriteLine("In polygon " + needVisable(layout2, new Point(17, 10)));
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -199,7 +476,7 @@ namespace KGLaba4
             if (currentLayout < image.GetLayoutCount())
             {
                 paintLayout(image.GetLayout(currentLayout++));
-                Console.WriteLine("ffff");
+                Console.WriteLine(currentLayout);
 
             }
             pictureBox1.Image = bitmap;
@@ -272,25 +549,28 @@ namespace KGLaba4
                 List<Point> inputList = new List<Point>(outputList);
                 outputList.Clear();
 
-                Point s = inputList[inputList.Count - 1];
-                foreach (Point e in inputList)
+                if (inputList.Count > 0)
                 {
-                    if (Inside(e, cp1, cp2))
+                    Point s = inputList[inputList.Count - 1];
+                    foreach (Point e in inputList)
                     {
-                        if (!Inside(s, cp1, cp2))
+                        if (Inside(e, cp1, cp2))
+                        {
+                            if (!Inside(s, cp1, cp2))
+                            {
+                                outputList.Add(Intersection(s, e, cp1, cp2));
+                            }
+                            outputList.Add(e);
+                        }
+                        else if (Inside(s, cp1, cp2))
                         {
                             outputList.Add(Intersection(s, e, cp1, cp2));
                         }
-                        outputList.Add(e);
+                        s = e;
                     }
-                    else if (Inside(s, cp1, cp2))
-                    {
-                        outputList.Add(Intersection(s, e, cp1, cp2));
-                    }
-                    s = e;
-                }
 
-                cp1 = cp2;
+                    cp1 = cp2;
+                }
             }
 
             return outputList;
