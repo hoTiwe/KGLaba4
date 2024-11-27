db73ade7886f1c4a94a6dae765aa532e2b78a2a0
Electr1k <110545080+Electr1k@users.noreply.github.com>
2024-11-27 23:29:07 +0300

    fiix

--- KGLaba4/Form1.Designer.cs
+++ KGLaba4/Form1.Designer.cs
@@ -56,6 +56,12 @@
             label8 = new Label();
             textBox9 = new TextBox();
             textBox10 = new TextBox();
+            label9 = new Label();
+            textBox11 = new TextBox();
+            label10 = new Label();
+            textBox12 = new TextBox();
+            label11 = new Label();
+            textBox13 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -315,11 +321,71 @@
             textBox10.TabIndex = 22;
             textBox10.Text = "0";
             // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(763, 384);
+            label9.Name = "label9";
+            label9.Size = new Size(14, 15);
+            label9.TabIndex = 31;
+            label9.Text = "B";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(751, 401);
+            textBox11.Margin = new Padding(3, 2, 3, 2);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(50, 23);
+            textBox11.TabIndex = 30;
+            textBox11.Text = "0";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(763, 334);
+            label10.Name = "label10";
+            label10.Size = new Size(15, 15);
+            label10.TabIndex = 29;
+            label10.Text = "G";
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(751, 351);
+            textBox12.Margin = new Padding(3, 2, 3, 2);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(50, 23);
+            textBox12.TabIndex = 28;
+            textBox12.Text = "0";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(764, 285);
+            label11.Name = "label11";
+            label11.Size = new Size(14, 15);
+            label11.TabIndex = 27;
+            label11.Text = "R";
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(752, 302);
+            textBox13.Margin = new Padding(3, 2, 3, 2);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(50, 23);
+            textBox13.TabIndex = 26;
+            textBox13.Text = "0";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(825, 450);
+            Controls.Add(label9);
+            Controls.Add(textBox11);
+            Controls.Add(label10);
+            Controls.Add(textBox12);
+            Controls.Add(label11);
+            Controls.Add(textBox13);
             Controls.Add(label7);
             Controls.Add(label8);
             Controls.Add(textBox9);
@@ -382,5 +448,11 @@
         private Label label8;
         private TextBox textBox9;
         private TextBox textBox10;
+        private Label label9;
+        private TextBox textBox11;
+        private Label label10;
+        private TextBox textBox12;
+        private Label label11;
+        private TextBox textBox13;
     }
 }
